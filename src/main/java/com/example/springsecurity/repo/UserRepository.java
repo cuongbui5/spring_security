cc51f6353887df38ff18977b337667d0501c4580
buicuong <cuongbui21503@gmail.com>
2023-09-04 16:49:51 +0700

    #1 - update user repository

--- src/main/java/com/example/springsecurity/repo/UserRepository.java
+++ src/main/java/com/example/springsecurity/repo/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.springsecurity.repo;
 
 import com.example.springsecurity.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 }
